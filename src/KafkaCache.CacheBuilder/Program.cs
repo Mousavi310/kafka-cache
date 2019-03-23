d40c34e54bd18b86be946709cccf489175110795
Morteza Mousavi <mousavi310@gmail.com>
2019-03-23 14:02:34 +0430

    Add cache builder project.

---
+++ src/KafkaCache.CacheBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KafkaCache.CacheBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
